7e1d16d7c36b0004a21cdea2e0c14c217d57f3a6
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-07 09:02:10 +0200

    Add project files.

---
+++ Galeotti_Semaforo_Binario/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Galeotti_Semaforo_Binario
+{
+    class Program
+    {
+        static int n = 0;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                n = 0;
+                Thread t1 = new Thread(() => Incrementa());
+                t1.Start();
+
+                Thread t2 = new Thread(() => Decrementa());
+                t2.Start();
+
+                while (t1.IsAlive) { }
+
+                while (t2.IsAlive) { }
+
+                Console.WriteLine(n);
+
+                Console.ReadLine();
+            }
+        }
+
+        private static void Incrementa()
+        {
+            for (int i = 0; i <= 1000000; i++)
+            {
+                n++;
+            }
+        }
+
+        private static void Decrementa()
+        {
+            for (int i = 0; i <= 1000000; i++)
+            {
+                n--;
+            }
+        }
+    }
+}
